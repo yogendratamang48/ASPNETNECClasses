7ba53d041154c5693d1f698a15eda36320d85b60
yogendratamang48 <yogen48@hotmail.com>
2016-09-06 09:00:10 +0545

    first commit

---
+++ SecondLecture/SecondLecture/Festival.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondLecture
+{
+    class Festival
+    {
+        string festivalName;
+        string priorityFor;
+        string isWorshipped;
+        Boolean isCollegeOffOnThatDay;
+
+        public void AcceptDetails()
+        {
+
+        }
+        public void DisplayDetails()
+        {
+
+        }
+
+    }
+}
---
+++ SecondLecture/SecondLecture/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondLecture
+{
+    class Rectangle
+    {
+        double length;
+        double width;
+        public void AcceptDetails()
+        {
+            Console.WriteLine("Enter Length:");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Width:");
+            width = Convert.ToDouble(Console.ReadLine());
+
+        }
+        public double GetArea()
+        {
+
+            return length * width;
+        }
+        public void Display()
+        {
+            double area = GetArea();
+            Console.WriteLine("Area: {0}*{1}={2} ",length, width, area);
+            Console.ReadKey();
+        }
+
+    }
+    class InteractWithReactangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rec = new Rectangle();
+            rec.AcceptDetails();
+            rec.Display();
+           
+        }
+    }
+}
